9176a921f4eb83cf0bbd34db677aebe3b587b74c
Travis Moret <tmoret058@gmail.com>
2020-12-13 19:41:44 -0600

    Fixed tabs v spaces issues and modified main to store prices in a dictionary for ratio printing.

--- client3.py
+++ client3.py
@@ -53,11 +53,13 @@ if __name__ == "__main__":
 
 	# Query the price once every N seconds.
    for _ in iter(range(N)):
-	   quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+      quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-	   """ ----------- Update to get the ratio --------------- """
-	   for quote in quotes:
-		   stock, bid_price, ask_price, price = getDataPoint(quote)
-		   print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+      """ ----------- Update to get the ratio --------------- """
+      prices = {}
+      for quote in quotes:
+         stock, bid_price, ask_price, price = getDataPoint(quote)
+         prices[stock] = price
+         print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-	   print ("Ratio %s" % getRatio(price, price))
+      print ("Ratio %s" % (getRatio(prices['ABC'], prices['DEF'])))
