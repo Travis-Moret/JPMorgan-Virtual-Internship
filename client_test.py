bd47d788a746fddd276062f99803b50f371b6413
Travis Moret <tmoret058@gmail.com>
2020-12-13 22:08:15 -0600

    Added unit testing for getRatio and getDataPoint. All tests passed.

--- client_test.py
+++ client_test.py
@@ -1,13 +1,17 @@
 import unittest
-from client3 import getDataPoint
+import random
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
+
   def test_getDataPoint_calculatePrice(self):
     quotes = [
       {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +19,25 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+       self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
+  
+  # Test getRatio with int values
+  def test_getRatio_1(self):
+      self.assertEqual(getRatio(1,2), 0.5)
 
+  # Test getRatio with the divide by 0 edge case
+  def test_getRatio_2(self):
+      self.assertEqual(getRatio(1,0), None)
 
-  """ ------------ Add more unit tests ------------ """
+  # Perform random testing on getRatio
+  def test_randomTesting_getRatio(self):
 
-
+      for _ in range(1000):
+         a = random.randint(1, 501)
+         b = random.randint(1, 501)
+         result = a/b
+         self.assertEqual(getRatio(a,b), result)
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
